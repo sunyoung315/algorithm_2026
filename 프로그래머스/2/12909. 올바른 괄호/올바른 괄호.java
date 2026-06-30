4e380c8638b8b2daa47b71226471ff575f26924e
sunyoung315 <139304927+sunyoung315@users.noreply.github.com>
2026-06-30 21:35:36 +0900

    [level 2] Title: 올바른 괄호, Time: 8.32 ms, Memory: 58 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            } else {
+                if(stack.isEmpty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
