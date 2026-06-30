708855b1fc41c69e4329dd3c8103978c98aaddb2
sunyoung315 <139304927+sunyoung315@users.noreply.github.com>
2026-06-30 11:20:41 +0900

    [level 1] Title: 폰켓몬, Time: 3.32 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+    
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n: nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        
+        return map.size() >= nums.length / 2 ? nums.length / 2 : map.size();
+    }
+}
